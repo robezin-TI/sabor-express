b32ace5dba8245f0e593de29afec7599a4acca90
robe <robert.domingues.97616@a.fecaf.com.br>
2025-08-21 18:46:34 -0300

    Create geo.py

---
+++ geo.py
@@ -0,0 +1,12 @@
+from geopy.geocoders import Nominatim
+
+_geocoder = Nominatim(user_agent="sabor_express")
+
+def geocode_address(address: str):
+    try:
+        loc = _geocoder.geocode(address, timeout=10)
+        if not loc:
+            return None
+        return (loc.latitude, loc.longitude)
+    except Exception:
+        return None
