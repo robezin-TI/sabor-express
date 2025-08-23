8d778dc10788722ae32155b7ad565435b6c8691b
robe <robert.domingues.97616@a.fecaf.com.br>
2025-08-23 12:49:40 -0300

    Update geo.py

--- geo.py
+++ geo.py
@@ -1,9 +1,11 @@
 import osmnx as ox
 
-
 def geocode_address(address: str):
+    """
+    Retorna (lat, lon, label) ou (None, None, 'Não encontrado')
+    """
     try:
-        location = ox.geocode(address)
-        return location[0], location[1], address
+        lat, lon = ox.geocode(address)
+        return float(lat), float(lon), address
     except Exception:
         return None, None, "Não encontrado"
