74f82a1ea21846487a656455322a5133c74b2b1d
robe <robert.domingues.97616@a.fecaf.com.br>
2025-08-25 14:35:43 -0300

    Update optimizer.py

--- api/optimizer.py
+++ api/optimizer.py
@@ -5,11 +5,7 @@ ox.settings.log_console = False
 ox.settings.use_cache = True
 
 def optimize_routes(points):
-    """Calcula rota ótima com A* entre os pontos fornecidos"""
-    if len(points) < 2:
-        return [], 0, 0
-
-    # monta grafo
+    """Calcula a melhor rota usando A*"""
     center = (points[0]["lat"], points[0]["lon"])
     G = ox.graph_from_point(center, dist=5000, network_type="drive")
 
@@ -19,14 +15,13 @@ def optimize_routes(points):
 
     for i in range(len(points) - 1):
         orig = ox.distance.nearest_nodes(G, points[i]["lon"], points[i]["lat"])
-        dest = ox.distance.nearest_nodes(G, points[i + 1]["lon"], points[i + 1]["lat"])
-
+        dest = ox.distance.nearest_nodes(G, points[i+1]["lon"], points[i+1]["lat"])
         path = nx.astar_path(G, orig, dest, weight="length")
         dist = sum(ox.utils_graph.get_route_edge_attributes(G, path, "length"))
-        time = dist / (40 * 1000 / 60)  # velocidade média 40km/h
+        time = dist / (40 * 1000 / 60)  # 40km/h
 
         route.extend(path)
         total_dist += dist
         total_time += time
 
-    return route, round(total_dist / 1000, 2), round(total_time, 2)
+    return route, round(total_dist/1000, 2), round(total_time, 2)
